74c31aa9e739a228ea6e30e6524d06669fb48b71
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-05-09 22:57:04 +0800

    Bug fixing for party_attend_num

--- app/controller/attendeeController.py
+++ app/controller/attendeeController.py
@@ -1,7 +1,17 @@
 from app.model.attendee import AttendeeModel
+from app.model.party import PartyModel
 
 
 def doAttendParty(attendee):
-    attendeeModel = AttendeeModel()
-    attendeeModel.insert(attendee=attendee)
-    return attendee['party_id']
+    party_id = attendee['party_id']
+    partyModel = PartyModel()
+    party = partyModel.find(party_id=party_id)
+    party_attend_num = party['party_attend_num']
+    party_total_num = party['party_total_num']
+    if party_attend_num < party_total_num:
+        party_attend_num = party_attend_num + 1
+        party['party_attend_num'] = party_attend_num
+        partyModel.update(party=party)
+        attendeeModel = AttendeeModel()
+        attendeeModel.insert(attendee=attendee)
+    return party_id
