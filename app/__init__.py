281910b6704f03c59d510376ccf8f795d487db71
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-05-23 05:49:31 +0800

    add session check for webpage

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'something is secret for others'
 
 from app.views import party
 from app.views import webpage
--- app/views/webpage.py
+++ app/views/webpage.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from flask import session
 from app.controller.wxsysController import getProgramList, getLicenseList
 from app.controller.wxsysController import updateProgram, doDeleteProgram
 from flask import request
@@ -7,7 +8,7 @@ from app.controller.wxsysController import addProgram, addLicense, deleteLicense
 from app.utils.util import getToday
 from app.controller.partyController import getAllPary
 
-@app.route('/helloworld')
+@app.route('/')
 def hello_world():
     return 'Hello World!'
 
@@ -17,14 +18,20 @@ def guild():
 
 @app.route('/party/partylist')
 def partyList():
-    partyList = getAllPary()
-    return render_template('partyplus/partylist.html', partyList=partyList)
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        partyList = getAllPary()
+        return render_template('partyplus/partylist.html', partyList=partyList)
+    else:
+        return goLogin()
 
 '''
 web page for 小程序管理
 '''
-@app.route('/')
+@app.route('/login')
 def goLogin():
+    session['isLogined'] = False
     template_name = 'wxsys/login.html'
     return render_template(template_name)
 
@@ -35,87 +42,140 @@ def doLogin():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
+
     if username == 'wzy' and password == 'zaq12wsx':
+        session['isLogined'] = True
         return showProgramList()
     else:
+        session['isLogined'] = False
         return goLogin()
 
 
 @app.route('/wxsysannounce')
 def wxsysAnnounce():
     template_name = 'wxsys/announce.html'
-    programList = getProgramList()
-    return render_template(template_name, programList=programList)
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        programList = getProgramList()
+        return render_template(template_name, programList=programList)
+    else:
+        return goLogin()
 
 @app.route('/dowxsysannounce', methods=['POST'])
 def doWxsysAnnounce():
-    if request.method == 'POST':
-        program_id = request.form.get('program_id')
-        program_message = request.form.get('program_message')
-        program = {'program_id' : program_id, 'program_message' : program_message}
-        updateProgram(program=program)
-    return showProgramList()
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        if request.method == 'POST':
+            program_id = request.form.get('program_id')
+            program_message = request.form.get('program_message')
+            program = {'program_id' : program_id, 'program_message' : program_message}
+            updateProgram(program=program)
+        return showProgramList()
+    else:
+        return goLogin()
 
 @app.route('/programlist')
 def showProgramList():
-    template_name = 'wxsys/programlist.html'
-    programList = getProgramList()
-    return render_template(template_name, programList=programList)
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        template_name = 'wxsys/programlist.html'
+        programList = getProgramList()
+        return render_template(template_name, programList=programList)
+    else:
+        return goLogin()
 
 @app.route('/newprogram')
 def goNewProgram():
-    template_name = 'wxsys/newprogram.html'
-    return render_template(template_name)
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        template_name = 'wxsys/newprogram.html'
+        return render_template(template_name)
+    else:
+        return goLogin()
 
 @app.route('/donewprogram', methods=['POST'])
 def doNewProgram():
-    if request.method == 'POST':
-        program_name = request.form.get('program_name')
-        program_id = request.form.get('program_id')
-        wxsys = {'program_id': program_id, 'program_name':program_name}
-        if program_name != '' and program_id!= '':
-            addProgram(wxsys=wxsys)
+    isLogined = session.get('isLogined')
 
-    return showProgramList()
+    if isLogined:
+        if request.method == 'POST':
+            program_name = request.form.get('program_name')
+            program_id = request.form.get('program_id')
+            wxsys = {'program_id': program_id, 'program_name':program_name}
+            if program_name != '' and program_id!= '':
+                addProgram(wxsys=wxsys)
+
+        return showProgramList()
+    else:
+        return goLogin
 
 @app.route('/deleteprogram', methods=['GET'])
 def deleteProgram():
-    if request.method == 'GET':
-        program_id = request.args.get('program_id')
-        doDeleteProgram(program_id=program_id)
+    isLogined = session.get('isLogined')
 
-    return showProgramList()
+    if isLogined:
+        if request.method == 'GET':
+            program_id = request.args.get('program_id')
+            doDeleteProgram(program_id=program_id)
+
+        return showProgramList()
+    else:
+        return goLogin()
 
 @app.route('/wxsyslicenselist')
 def wxsysLicenseList():
-    template_name = 'wxsys/licenselist.html'
-    licenseList = getLicenseList()
-    today = getToday()
-    return render_template(template_name, licenseList=licenseList)
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        template_name = 'wxsys/licenselist.html'
+        licenseList = getLicenseList()
+        today = getToday()
+        return render_template(template_name, licenseList=licenseList)
+    else:
+        return goLogin()
 
 @app.route('/wxsysnewlicense')
 def wxsysNewLicense():
-    template_name = 'wxsys/newlicense.html'
-    programList = getProgramList()
-    today = getToday()
-    return render_template(template_name, programList=programList, today=today)
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        template_name = 'wxsys/newlicense.html'
+        programList = getProgramList()
+        today = getToday()
+        return render_template(template_name, programList=programList, today=today)
+    else:
+        return goLogin()
 
 @app.route('/dowxsysnewlicense', methods=['POST'])
 def doWxsysNewLicense():
-    license = {}
-    if request.method == 'POST':
-        license['program_id'] = request.form.get('program_id')
-        license['company_name'] = request.form.get('company_name')
-        license['tel_no'] = request.form.get('tel_no')
-        license['license_start_date'] = request.form.get('license_start_date')
-        license['license_period'] = request.form.get('license_period')
-        addLicense(license=license)
-    return wxsysLicenseList()
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        license = {}
+        if request.method == 'POST':
+            license['program_id'] = request.form.get('program_id')
+            license['company_name'] = request.form.get('company_name')
+            license['tel_no'] = request.form.get('tel_no')
+            license['license_start_date'] = request.form.get('license_start_date')
+            license['license_period'] = request.form.get('license_period')
+            addLicense(license=license)
+        return wxsysLicenseList()
+    else:
+        return goLogin()
 
 @app.route('/deletelicense', methods=['GET'])
 def doDeleteLicense():
-    license = {}
-    if request.method == 'GET':
-        license_num = request.args.get('license_num')
-        deleteLicense(license_num=license_num)
-    return wxsysLicenseList()
+    isLogined = session.get('isLogined')
+
+    if isLogined:
+        license = {}
+        if request.method == 'GET':
+            license_num = request.args.get('license_num')
+            deleteLicense(license_num=license_num)
+        return wxsysLicenseList()
+    else:
+        return goLogin()
